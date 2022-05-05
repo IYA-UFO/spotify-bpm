ba371a704c37026f7115e243038e64ea0f2b2eb5
IYA-U <36294547+IYA-U@users.noreply.github.com>
2022-05-05 14:40:42 +0900

    Fix Bugs

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
